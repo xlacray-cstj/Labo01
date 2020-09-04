2d2fb65ab37eb89669279043fcd834490f599ea6
Xavier L-R <2043249@cstj.qc.ca>
2020-09-04 13:42:44 -0400

    Deuxième commit / ajout de iostream

--- ProjetWow/Test.cpp
+++ ProjetWow/Test.cpp
@@ -4,10 +4,15 @@
 // Auteur :  Xavier Lacroix-Raymond
 // Date : 4-09-2020
 
+#include <iostream>		// Intègre le coffre à outils pour afficher à l'écran (o out output) (i in input)
+
+
+
 int main(/* liste des paramètres séparés par une virgule le cas échéant */)
 {
 
-
+	// Affichage du message à l'écran
+	std::cout << "Steak Blé d'Inde Patates";
 
 
 	return 0;
