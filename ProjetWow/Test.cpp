7f0e8619906f61e907529a5618e9420c6e602a87
Xavier L-R <2043249@cstj.qc.ca>
2020-09-04 12:59:00 -0400

    Ajoutez des fichiers projet.

---
+++ ProjetWow/Test.cpp
@@ -0,0 +1,5 @@
+// But : Créer un programme qui affiche un message à l'écran en sauvegardant plusieurs versions de
+//		 ce programme avec git et de l'héberger sur la plateforme GitHub
+//
+// Auteur :  Xavier Lacroix-Raymond
+// Date : 4-09-2020
